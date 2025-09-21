a9ff3bc8d4846112fe0b241f2911f58fd22f8584
ozarka30 <ozarka30@gmail.com>
2025-09-20 21:36:34 -0500

    Working Ghost

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758421521|155388575';
+const CACHE_VERSION = '1758422181|814820626';
 /** @type {string} */
 const CACHE_PREFIX = 'Critter-Prototyp-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

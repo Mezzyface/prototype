132ff3249d4b8f8e3da419d84193b21a94c3d9e6
ozarka30 <ozarka30@gmail.com>
2025-09-20 22:51:04 -0500

    New Runnable

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758422181|814820626';
+const CACHE_VERSION = '1758426657|3439686957';
 /** @type {string} */
 const CACHE_PREFIX = 'Critter-Prototyp-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
